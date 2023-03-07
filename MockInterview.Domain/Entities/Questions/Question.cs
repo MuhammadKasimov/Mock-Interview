6146976fa07cdec8d2c812a2e7ea9c37163dd8e0
Temurkhann <temurxonshodiyev4@gmail.com>
2023-03-07 16:35:17 +0500

    DATA: DbContext created

--- MockInterview.Domain/Entities/Questions/Question.cs
+++ MockInterview.Domain/Entities/Questions/Question.cs
@@ -1,10 +1,6 @@
 ﻿using MockInterview.Domain.Commons;
 using MockInterview.Domain.Entities.Quizzes;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MockInterview.Domain.Entities.Questions
 {
